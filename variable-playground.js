1c1126b96b29407e12762e93f3189f27b73aa1f2
Helder Pedro <pedrolance@gmail.com>
2016-07-07 13:21:50 +0100

    Add variable-playground file

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name : 'Helder',
+// 	age : 21,
+// };
+
+// function updatePerson (obj) {
+// 	//obj = {
+// 	//	name : 'Helder',
+// 	//	age : 37
+// 	//}
+// 	obj.age = 24;
+// };
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [ 39, 21];
+
+function addGrades (gradesArr, newGrade) {
+	gradesArr.push(newGrade);
+	debugger;
+};
+
+addGrades(grades, 3);
+console.log(grades);
