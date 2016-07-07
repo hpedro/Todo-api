172dde9242699959bd2b5ec94f51eda28fb9e2bd
Helder Pedro <pedrolance@gmail.com>
2016-07-07 11:43:59 +0100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [];
+var todos = [ ];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
@@ -23,6 +23,7 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
+	console.log(todos);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
@@ -48,6 +49,21 @@ app.post('/todos', function(req,res) {
 	todos.push(body);
 	res.json(body);
 
+});
+
+// DELETE /todos/:id
+
+app.delete('/todos/:id', function(req,res) {
+	var todoId = parseInt(req.params.id, 10);
+
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function () {
