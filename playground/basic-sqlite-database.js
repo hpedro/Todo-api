bff2631ed623177b3b8b5c5f5e12026560105e40
Helder Pedro <pedrolance@gmail.com>
2016-07-12 10:39:51 +0100

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,45 +19,43 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//	force: true
-	}).then(function() {
+}).then(function() {
 	console.log('Everything is synced');
 
-	Todo.findById(3).then(function(todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('todo not found')
-		}
+	User.findById(1).then( function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then( function(todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
 
-	// Todo.create({
-	// 	description: "Work all night",
-	// }).then(function(todo) {
+	// User.create({
+	// 	email: 'hpedro@gmail.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: "Clean Office"
+	// 		description: "Clean yard"
 	// 	});
-	// }).then(function() {
-	// 	//return Todo.findById(1);
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%office%'
-	// 			}
-	// 		}
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
 	// 	});
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function (todo) {
-	// 			console.log(todo.toJSON());
-	// 		})
-	// 	} else {
-	// 		console.log('no todos found');
-	// 	}
-	// }).catch(function(e){
-	// 	console.log(e);
 	// });
-
 });
+
+
+
 
